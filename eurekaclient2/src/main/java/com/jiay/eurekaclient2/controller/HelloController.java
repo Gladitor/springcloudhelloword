e59134a6ef1aa35ac83af903868b1d1ee7426a45
jiay <jiay@localhost.localdomain>
2020-03-12 16:36:37 +0800

    eurekaclient2.yml management

--- eurekaclient2/src/main/java/com/jiay/eurekaclient2/controller/HelloController.java
+++ eurekaclient2/src/main/java/com/jiay/eurekaclient2/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.jiay.eurekaclient2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
